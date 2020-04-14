7968721f7f47cbc31b9023768572eaa5bd2e1864
Tina Chai <thh258369@gmail.com>
2020-04-14 20:11:13 +0200

    Updated version

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('Guess the number: ') #input 要存在(=) num 裡面 
-	num = int(num) #因為要猜的是數字，所以要讓num變整數(casting 型別轉換)
+	num = int(num) 
 	if num == r:
 		print('You win!')
+		print('This is the', count, 'time.')
 		break
 	elif num > r: #另外如果
 		print('It is bigger than answer.')
 	elif num < r:
 		print('It is smaller than answer.')
-
+	print('This is the', count, 'time.')
